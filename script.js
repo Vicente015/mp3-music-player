7c33fba82375061e7990105641949c1ab9cbf34b
Sax <abelfalconespino@gmail.com>
2024-01-18 23:05:40 +0100

    Update audio player UI and functionality

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+/**
+ * @type {Object}
+ */
 let song
+/**
+ * @type {HTMLAudioElement}
+ */
 let audio
 
 const songs = await(await fetch('./songs.json')).json()
 
 function init () {
-  /**
-   * @type {Object[]}
-   */
   console.debug(songs)
 
   const random = Math.round(Math.random() * songs.length)
@@ -14,87 +17,101 @@ function init () {
 
   audioBars()
   progressBar()
+  changeArtwork()
 }
 
 function playSong (newIndex) {
-  if (audio) audio.pause()
+  const timeUpdateListener = () => {
+    progressBar()
+    audioBars()
+  }
+
+  if (audio) {
+    audio.pause()
+    audio.removeEventListener('timeupdate', timeUpdateListener)
+  }
+
+  console.debug('newIndex', newIndex)
+
   song = songs[newIndex]
   audio = new Audio(song.url)
   audio.play()
 
-
   const title = document.getElementById('title')
   const autor = document.getElementById("autor")
 
+  // Reiniciar valores
   title.innerText = song.title
   autor.innerText = song.artist
 
-  console.log(song.image)
+  audio.addEventListener('timeupdate', timeUpdateListener)
 
+  changeArtwork(song.image);
+
+  console.log(song.image)
 }
 
 
 const audioBars = () => {
-  audio.addEventListener('timeupdate', () => {
-    const currentTime = audio.currentTime
-    const duration = audio.duration
+  const currentTime = audio.currentTime
+  const duration = audio.duration
+  const currentMinutes = Math.floor(currentTime / 60)
+  const currentSeconds = Math.floor(currentTime % 60)
+  const durationMinutes = Math.floor(duration / 60)
+  const durationSeconds = Math.floor(duration % 60)
+  const currentTimeFormatted = `${currentMinutes}:${currentSeconds.toString().padStart(2, '0')}`
+  const durationFormatted = `${durationMinutes}:${durationSeconds.toString().padStart(2, '0')}`
 
-    const currentMinutes = Math.floor(currentTime / 60)
-    const currentSeconds = Math.floor(currentTime % 60)
-    const durationMinutes = Math.floor(duration / 60)
-    const durationSeconds = Math.floor(duration % 60)
-
-    const currentTimeFormatted = `${currentMinutes}:${currentSeconds.toString().padStart(2, '0')}`
-    const durationFormatted = `${durationMinutes}:${durationSeconds.toString().padStart(2, '0')}`
-
-    document.getElementById('currentTime').innerText = currentTimeFormatted
-    document.getElementById('duration').innerText = durationFormatted
-
-  })
+  document.getElementById('currentTime').innerText = currentTimeFormatted
+  document.getElementById('duration').innerText = durationFormatted
 }
+
 
 const progressBar = () => {
+  const control_volumen = document.getElementById('control_volumen')
+  const currentTime = audio.currentTime
+  const duration = audio.duration
+  const progress = Math.floor((currentTime * 100) / duration)
 
-  audio.addEventListener('timeupdate', () => {
-    const currentTime = audio.currentTime
-    const duration = audio.duration
-
-    const progress = Math.round((currentTime * 100) / duration)
-
-    const control_volumen = document.getElementById('control_volumen')
-    control_volumen.value = progress
-
-    console.log(progress)
-
-  });
+  control_volumen.value = progress
 }
 
-// const artboxInage = () => {
-//   const artboxInage = song.image
-//   document.getElementById('.art-box').style.backgroundImage = `url(${artboxInage})`
-// }
-
 function previous () {
-  const songIndex = songs.indexOf(song)
-  if (songIndex <= 0) {
+  const songIndex = songs.indexOf(song) - 1
+  if (songIndex < 0) {
     playSong(songs.length - 1)
   } else {
-    playSong(songIndex - 1)
+    playSong(songIndex)
   }
 }
 
 const play = () => {
-  audio.paused == true ? audio.play() : audio.pause()
+  const audioPausado = audio.paused === true
+  audioPausado ? audio.play() : audio.pause()
+  const playBtn = document.getElementById('playBtn')
+  const pauseBtn = document.getElementById('pauseBtn')
+
+  playBtn.classList.toggle('hidden')
+  pauseBtn.classList.toggle('hidden')
+
+  changeArtwork(song.image);
+
 }
 
 const next = () => {
-  const songIndex = songs.indexOf(song)
+  const songIndex = songs.indexOf(song) + 1
   if (songIndex >= songs.length) {
     playSong(0)
   } else {
-    playSong(songIndex + 1)
+    playSong(songIndex)
   }
 }
+
+const changeArtwork = (artworkUrl) => {
+  const artwork = document.getElementById('art-box')
+  artwork.src = artworkUrl
+  artwork.style.background = `url(${artworkUrl})`
+};
 
 document.getElementById('previous')
   .addEventListener('click', previous)
@@ -104,8 +121,6 @@ document.getElementById('play')
 
 document.getElementById('next')
   .addEventListener('click', next)
-
-
 
 init()
 
